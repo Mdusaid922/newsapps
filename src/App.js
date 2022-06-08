b9542644ae2a60cb969fb6344c098ee70d375325
usaid <mdusaid922@gmail.com>
2022-06-08 17:40:34 +0500

    inital

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import NavBar from "./components/NavBar";
+import News from "./components/News";
+import {
+  BrowserRouter,
+  BrowserRouter as Route,Routes
+} from "react-router-dom";
+import Footer from "./components/Footer";
+
+export default class App extends Component {
+  pageSize=12;
+  render() {
+    return (
+      <div style={{ backgroundColor: " #9E8015 " }}>
+        <BrowserRouter>
+          <NavBar />
+          <Routes>
+            <Route exact path="/" element=              {<News key="general"        pageSize={this.pageSize} country="us" category="general" />} />
+            <Route exact path="/business" element=      {<News key="business"       pageSize={this.pageSize} country="us" category="business" />} />
+            <Route exact path="/Entertainment" element= {<News key="Entertainment"  pageSize={this.pageSize} country="us" category="Entertainment" />} />
+            <Route exact path="/general" element=       {<News key="general"        pageSize={this.pageSize} country="us" category="general" />} />
+            <Route exact path="/health" element=        {<News key="health"         pageSize={this.pageSize} country="us" category="health" />} />
+            <Route exact path="/science" element=       {<News key="science"        pageSize={this.pageSize} country="us" category="science" />} />
+            <Route exact path="/sports" element=        {<News key="sports"         pageSize={this.pageSize} country="us" category="sports" />} />
+            <Route exact path="/technology" element=    {<News key="technology"     pageSize={this.pageSize} country="us" category="technology" />} />
+          </Routes>
+        </BrowserRouter>
+        <Footer/>
+      </div>
+    );
+  }
 }
 
-export default App;
